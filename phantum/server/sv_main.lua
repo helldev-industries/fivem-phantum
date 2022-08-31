5a02531c4e48ca58247bfb69f4ff624bf996567d
Phantum <phant@helldev.ga>
2022-08-31 18:57:14 +0200

    (fix) cfx api is shit

--- phantum/server/sv_main.lua
+++ phantum/server/sv_main.lua
@@ -13,14 +13,14 @@ end
 PhantumUtils.Tick = function(a, b)
 	local fn, t;
 	
-	if type(a) == 'function' then fn = a else t = a end;
-	if type(b) == 'function' then fn = b else t = b end;
+	if type(a) == 'function' or type(a) == 'table' then fn = a else t = a end;
+	if type(b) == 'function' or type(b) == 'table' then fn = b else t = b end;
 	if type(t) ~= 'number' then t = 0; end;
 	
     PhantumUtils.Thread(function()
         while true do
-            Wait(t)
-            fn()
+            Wait(t);
+            fn();
         end
     end)
 end
