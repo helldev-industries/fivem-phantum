fb0e28d97d49b2f4a9580f4a82d481f15fd19adb
Phantum <phant@helldev.ga>
2022-08-31 18:37:27 +0200

    (f) PhantumUtils.Tick better api

--- phantum/client/cl_main.lua
+++ phantum/client/cl_main.lua
@@ -4,12 +4,17 @@ PhantumUtils.Thread = function(fn)
     Citizen.CreateThread(fn)
 end
 
-PhantumUtils.Tick = function(time, fn)
-	if not time then time = 0 end;
+PhantumUtils.Tick = function(a, b)
+	local fn, t;
+	
+	if type(a) == 'function' then fn = a else t = a end;
+	if type(b) == 'function' then fn = b else t = b end;
+	if type(t) ~= 'number' then t = 0; end;
+	
     PhantumUtils.Thread(function()
         while true do
-            Citizen.Wait(time)
-            fn()
+            Wait(t);
+            fn();
         end
     end)
 end
--- phantum/server/sv_main.lua
+++ phantum/server/sv_main.lua
@@ -10,11 +10,16 @@ PhantumUtils.Thread = function(fn)
     CreateThread(fn)
 end
 
-PhantumUtils.Tick = function(time, fn)
-	if not time then time = 0 end;
+PhantumUtils.Tick = function(a, b)
+	local fn, t;
+	
+	if type(a) == 'function' then fn = a else t = a end;
+	if type(b) == 'function' then fn = b else t = b end;
+	if type(t) ~= 'number' then t = 0; end;
+	
     PhantumUtils.Thread(function()
         while true do
-            Wait(time)
+            Wait(t)
             fn()
         end
     end)
